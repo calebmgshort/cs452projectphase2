e016de862f15048e297f8eba414e95e461c8e5a9
Caleb Short <Caleb@Calebs-MBP-2.PK5001Z>
2017-09-28 21:16:26 -0700

    Implemented diskHandler

--- handler.c
+++ handler.c
@@ -25,10 +25,12 @@ void clockHandler2(int type, void* unitPointer)
     if(type != USLOSS_CLOCK_DEV)
     {
         USLOSS_Console("clockHandler2(): called with wrong type %d\n", type);
+        USLOSS_Halt(1);
     }
     if(unit != 0)
     {
         USLOSS_Console("clockHandler2(): called with invalid unit number %d\n", unit);
+        USLOSS_Halt(1);
     }
 
     timeSlice();
@@ -48,10 +50,27 @@ void clockHandler2(int type, void* unitPointer)
 void diskHandler(int dev, int unit)
 {
 
+    int unit = *((int*) unitPointer);
+
     if (DEBUG2 && debugflag2)
     {
         USLOSS_Console("diskHandler(): called\n");
     }
+    if(type != USLOSS_DISK_DEV)
+    {
+        USLOSS_Console("diskHandler(): called with wrong type %d\n", type);
+        USLOSS_Halt(1);
+    }
+    if(unit != 0 && unit != 1)
+    {
+        USLOSS_Console("diskHandler(): called with invalid unit number %d\n", unit);
+        USLOSS_Halt(1);
+    }
+    int mboxID = getDeviceMboxID(type, unit);
+    int* status;
+    *status = -1;
+    USLOSS_DeviceInput(dev, unit, status);
+    MboxCondSend(mboxID, (void*) status, sizeof(int));
 
 } /* diskHandler */
 
